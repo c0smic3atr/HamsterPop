c842c36fda10c050d6d46bbb1715155581c012d5
c0smic3atr <marsdatrandomdemon@gmail.com>
2024-12-17 08:16:23 -0800

    storage script created

---
+++ Assets/Scripts/Storage.cs
@@ -0,0 +1,14 @@
+public static class Storage
+{
+    static int lives = 3;
+
+    public static int GetLives()
+    {
+        return lives;
+    }
+
+    public static void SetLives(int newLives)
+    {
+        lives = newLives;
+    }
+}
